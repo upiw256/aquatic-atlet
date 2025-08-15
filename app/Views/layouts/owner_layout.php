33141d8761aea93a7ad68756578161cb296c2cd8
upiw256 <bilqimlb@gmail.com>
2025-08-15 23:29:20 +0700

    perbaiki gitignore dan biodata pada owner

--- app/Views/layouts/owner_layout.php
+++ app/Views/layouts/owner_layout.php
@@ -75,6 +75,19 @@
                 }
             });
         }
+
+        function previewPhoto(event) {
+            const input = event.target;
+            const preview = document.getElementById('photo-preview');
+
+            if (input.files && input.files[0]) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    preview.src = e.target.result;
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
     </script>
 </body>
 
